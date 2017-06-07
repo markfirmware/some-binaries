f29ecc25bcb8bc6de6796e4c8186069b0df7fe82
markfirmware <markfirmware@users.noreply.github.com>
2017-06-07 21:26:23 +0000

    need host ip parameter

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-qemu-system-arm -monitor stdio -M versatilepb -cpu cortex-a8 -kernel kernel.bin -m 256M -serial file:serial0.txt -usb -net nic -net user,hostfwd=tcp::5578-:80 -vnc :78,websocket=5778 -append 'qemuhostip=51.15.141.116 qemuhostportdigit=8'
+qemu-system-arm -monitor stdio -M versatilepb -cpu cortex-a8 -kernel kernel.bin -m 256M -serial file:serial0.txt -usb -net nic -net user,hostfwd=tcp::5578-:80 -vnc :78,websocket=5778 -append 'qemuhostip=212.47.231.140 qemuhostportdigit=8'
--- install-ultibo-scaleway-debian-jessie.sh
+++ install-ultibo-scaleway-debian-jessie.sh
@@ -2,5 +2,6 @@
 
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y git qemu-system-arm/jessie-backports
+apt-get install -y fail2ban git qemu-system-arm/jessie-backports
 qemu-system-arm --version
+wget https://github.com/markfirmware/some-binaries/releases/download/1.00/kernel.bin
